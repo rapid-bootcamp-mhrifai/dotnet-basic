3f17cc9b788b5e2d0c06d4bb8923a64ce2c89664
mhrifai <mkhrifai99@gmail.com>
2023-01-02 14:07:42 +0700

    logic2 soal 1, 2, 3

---
+++ DotnetBasic/Logic02/Logic02Soal02.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDasar
+{
+    public class Logic02Soal02
+    {
+        public Logic02Soal02()
+        {
+        }
+
+        public static void CetakData(int n)
+        {
+            Console.WriteLine("Logic02Soal02" + "\n");
+            for (int i = 0; i < n; i++)
+            {
+                int angka = 1;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0 || i == 0 || i == n - 1 || j == n - 1)
+                    {
+                        Console.Write(angka + "\t");
+                    }
+                    else
+                    {
+                        Console.Write("\t");
+                    }
+
+                    angka += 2;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
