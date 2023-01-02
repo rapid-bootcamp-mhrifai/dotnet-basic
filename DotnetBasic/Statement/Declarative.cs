6001f5534f5af777321a8e4ce274bab1d0247163
mhrifai <mkhrifai99@gmail.com>
2023-01-02 11:24:05 +0700

    introduction

---
+++ DotnetBasic/Statement/Declarative.cs
@@ -0,0 +1,16 @@
+﻿public class Declarative
+{
+    public static void SampleDeclarative()
+    {
+        Console.WriteLine("Declarative Test");
+
+        double area = 0.0;
+        Console.WriteLine("Area Value : " + area);
+
+        double radius = 2;
+        Console.WriteLine("Radius value : "+ radius);
+
+        const double pi = 3.14159;
+        Console.WriteLine("Pi value : "+pi);
+    }
+}
---
+++ DotnetBasic/Statement/MainStatement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Statement
+{
+    public class MainStatement
+    {
+        public MainStatement()
+        {
+        }
+
+        public static void Main()
+        {
+            Declarative.SampleDeclarative();
+            ForStatement.SampleFor();
+        }
+    }
+}
